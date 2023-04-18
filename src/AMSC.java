f18cb4a5845d9061158dd8c6452a36e1c7ddad50
Sarah Cyphers <cyphers.18@buckeyemail.osu.edu>
2023-04-18 15:24:40 -0400

    Formatted principle, monthly payment and added balance

--- src/AMSC.java
+++ src/AMSC.java
@@ -10,9 +10,8 @@ public class AMSC {
         System.out.printf("Enter loan amount: ");
         double loanAmount = input.nextDouble();
 
-        System.out.print("Enter length of loan: ");
+        System.out.print("Enter length of loan in months: ");
         int loanLength = input.nextInt();
-        int periods = loanLength * 12;
 
         System.out.print("Enter interest rate: ");
         double interestRate = input.nextDouble();
@@ -21,27 +20,29 @@ public class AMSC {
         input.close();
 
         
-        double monthlyPayment = loanAmount * monthlyRate / (1 - 1 / Math.pow(1 + monthlyRate, periods)); 
+        double monthlyPayment = loanAmount * monthlyRate / (1 - 1 / Math.pow(1 + monthlyRate, loanLength)); 
 
         double interestPaid = loanAmount * monthlyRate;
 
         double principleAmount = monthlyPayment - interestPaid;
         double interest;
+        double balance = loanAmount;
 
         // format output
-        System.out.println("Month\t\tLoan Amount\t\tInterest\t\tPrinciple\t\tMonthly Payment");
+        System.out.println("Month\t\tLoan Amount\t\tInterest\t\tPrinciple\t\tPayment\t\tBalance");
         System.out.println("");
 
-        for (int i = 1; i <= periods; i++) {
+        for (int i = 1; i <= loanLength; i++) {
             interest = loanAmount * monthlyRate;
             principleAmount = monthlyPayment - interest;
-            formatPrint(i, loanAmount, interest, principleAmount, monthlyPayment);
+            balance = loanAmount - principleAmount;
+            formatPrint(i, loanAmount, interest, principleAmount, monthlyPayment, balance);
             loanAmount = loanAmount - principleAmount;
         }
 
     }
  
-    private static void formatPrint(int i, double loanAmount, double interest, double principleAmount, double monthlyPayment) {
+    private static void formatPrint(int i, double loanAmount, double interest, double principleAmount, double monthlyPayment, double balance) {
        
         DecimalFormat df = new DecimalFormat("$#,###.00");
 
@@ -64,12 +65,50 @@ public class AMSC {
         else {
             System.out.print(df.format(loanAmount));
         }
-        System.out.println("\t\t");
+        System.out.print("\t\t");
 
+        //format interest
+       if (interest < 10) {
+            System.out.print(df.format(interest) + " ");
+            System.out.print("\t\t\t");
+        }
+        else if (interest > 999) {
+            System.out.print(df.format(interest));
+            System.out.print("\t\t");
+        }
+        else {
+            System.out.print(df.format(interest));
+            System.out.print("\t\t\t");
+        }
 
+        // format principle
+       if (principleAmount < 1000) {
+            if (principleAmount <10) {
+                System.out.print(df.format(principleAmount) + "    ");
+            }
+            else {
+                System.out.print(df.format(principleAmount) + "  ");
+            }
+        }
+        else {
+            System.out.print(df.format(principleAmount));
+        }
+        System.out.print("\t\t");
 
+        // format monthly payment
+        if (monthlyPayment > 999) {
+            System.out.print(df.format(monthlyPayment));
+            System.out.print("\t");
+        }
+        else {
+            System.out.print(df.format(monthlyPayment));
+            System.out.print("\t\t");
+        }
 
-                                        
+        // format balance
+        System.out.println(df.format(balance));
+        
+                              
     }
 }
     
